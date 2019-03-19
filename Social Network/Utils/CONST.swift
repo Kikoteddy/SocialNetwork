3403ebed51b54c859ed463603f502f005b902cb5
Kristijan Ivanov <kikoodskopje@hotmail.com>
2019-03-19 13:44:25 +0100

    sign in ui custom classes complete

---
+++ Social Network/Utils/CONST.swift
@@ -0,0 +1,11 @@
+//
+//  CONST.swift
+//  Social Network
+//
+//  Created by Teddy on 3/19/19.
+//  Copyright © 2019 a. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ Social Network/View/RoundFbBtn.swift
@@ -0,0 +1,30 @@
+//
+//  RoundFbBtn.swift
+//  Social Network
+//
+//  Created by Teddy on 3/19/19.
+//  Copyright © 2019 a. All rights reserved.
+//
+
+import UIKit
+
+class RoundFbBtn: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        imageView?.contentMode = .scaleAspectFit
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+        
+    }
+
+}
